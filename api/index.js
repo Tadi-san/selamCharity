45b79bfa4621a767088daaca47b626d22933cfd9
Tadi-san <tadiosanegagregn@yahoo.com>
2023-11-16 22:42:33 +0300

    M

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ app.use('/uploads', express.static(__dirname + "/uploads"))
 app.use(express.json())
 app.use(cors({
     credentials: true,
-    origin: "http://localhost:5173"
+    origin: "https://selam-charity-for-better-future.vercel.app/"
 }))
 const { MongoClient } = require("mongodb");
 const adminName = 'abel'
